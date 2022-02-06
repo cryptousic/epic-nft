b1b1ba3d4b9974f47b6feb6b4792f7eb8fe712b1
cryptousic <natan.usic@protonmail.com>
2022-02-06 20:53:38 +0100

    changed number of maximum minted NFTs

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -12,7 +12,7 @@ contract MyEpicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     uint256 totalNFTs;
-    uint constant maximumMintedNFTs = 50;
+    uint constant maximumMintedNFTs = 6;
 
     string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
